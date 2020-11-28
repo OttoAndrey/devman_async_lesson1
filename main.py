307888f06ce18552668345ca083d968a459f813e
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-11-28 15:18:57 +0700

    Add borders

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import time
-import curses
 import asyncio
+import curses
+import os
 import random
+import time
 from itertools import cycle
-from curses_tools import draw_frame, read_controls
+from statistics import median
+
+from curses_tools import draw_frame, read_controls, get_frame_size
 
 
 async def animate_spaceship(canvas, start_row, start_column, frames):
     row, column = start_row, start_column
+    max_rows, max_columns = canvas.getmaxyx()
+    max_rows -= 1
+    max_columns -= 1
+
     for frame in cycle(frames):
         rows_direction, columns_direction, space_pressed = read_controls(canvas)
+        frame_rows, frames_cols = get_frame_size(frame)
         row += rows_direction
         column += columns_direction
+
+        row = median([1, row, max_rows-frame_rows])
+        column = median([1, column, max_columns-frames_cols])
+
         draw_frame(canvas, row, column, frame)
         await asyncio.sleep(0)
         draw_frame(canvas, row, column, frame, negative=True)
@@ -50,7 +62,7 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(0, 4):
+        for _ in range(0, 3):
             await asyncio.sleep(0)  
 
         canvas.addstr(row, column, symbol)
@@ -68,28 +80,27 @@ async def blink(canvas, row, column, symbol='*'):
 
 def draw(canvas):
     canvas.nodelay(True)
-    with open("frames/rocket_frame_1.txt", "r") as my_file:
-        rocket_frame_1 = my_file.read()
-
-    with open("frames/rocket_frame_2.txt", "r") as my_file:
-        rocket_frame_2 = my_file.read()
-
-    frames = [rocket_frame_1, rocket_frame_2]
-
     curses.curs_set(False)
     stars = '*:+.'
-    max_row, max_col = canvas.getmaxyx()
-
+    max_rows, max_cols = canvas.getmaxyx()
     coroutines = []
-    coroutine_spaceship = animate_spaceship(canvas, max_row/2, max_col/2, frames)
-    coroutine_fire = fire(canvas, max_row-2, (max_col-2)/2)
+    frames = []
+
+    rocket_frames_path = 'frames/rocket_frames/'
+    rocket_frames_names = os.listdir(rocket_frames_path)
+    for rocket_frames_name in rocket_frames_names:
+        with open(f'{rocket_frames_path}{rocket_frames_name}', 'r') as frame:
+            frames.append(frame.read())
+
+    coroutine_spaceship = animate_spaceship(canvas, max_rows/2, max_cols/2, frames)
+    coroutine_fire = fire(canvas, max_rows-2, (max_cols-2)/2)
 
     coroutines.append(coroutine_spaceship)
     coroutines.append(coroutine_fire)
 
     for _ in range(0, 100):
-        row = random.randint(1, max_row-2)
-        column = random.randint(1, max_col-2)
+        row = random.randint(1, max_rows-2)
+        column = random.randint(1, max_cols-2)
         coroutine_blink = blink(canvas, row, column, random.choice(stars))
         coroutines.append(coroutine_blink)
 
